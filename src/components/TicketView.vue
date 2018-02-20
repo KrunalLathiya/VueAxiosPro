10b97575ca9623a82c0f48ba4fb51d3f584b5bef
KrunalLathiya <krunallathiya21@gmail.com>
2018-02-21 02:45:58 +0530

    Vue axios tutorial

---
+++ src/components/TicketView.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <h1>Tickets</h1>
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <td>ID</td>
+                <td>Email ID</td>
+                <td>Description</td>
+            </tr>
+            </thead>
+            <tbody>
+               <tr v-for="ticket in tickets" :key="ticket._id">
+                    <td>{{ ticket._id }}</td>
+                    <td>{{ ticket.email }}</td>
+                    <td>{{ ticket.description }}</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data(){
+            return{
+                tickets: []
+            }
+        },
+
+        created: function()
+        {
+            this.fetchItems();
+        },
+
+        methods: {
+            fetchItems()
+            {
+              let uri = 'http://localhost:4000/tickets';
+              this.axios.get(uri).then((response) => {
+                  this.tickets = response.data;
+              });
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import AddTicket from './components/AddTicket.vue';
+import TicketView from './components/TicketView.vue';
 
-Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
+Vue.use(VueRouter);
+
+const routes = [
+  {
+        name: 'add',
+        path: '/add',
+        component: AddTicket
+  },
+  {
+    name: 'index',
+    path: '/index',
+    component: TicketView
+}
+];
+const router = new VueRouter({ mode: 'history', routes: routes });
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
